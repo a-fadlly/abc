28006094d66c671e0ce56e0615d5e47cffcda798
Daimaru Tape <Daimaru Tape@DESKTOP-VD3I5ME>
2023-03-27 11:48:56 +0700

    Rewrite

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -56,7 +56,7 @@
         </div>
         <!-- Image Section -->
         <div class="w-1/2 shadow-2xl">
-            <img class="object-cover w-full h-screen hidden md:block" src="https://source.unsplash.com/IXUM4cJynP0">
+            <img class="object-cover w-full h-screen hidden md:block" src="https://source.unsplash.com/6dW3xyQvcYE">
         </div>
     </div>
 </body>
