72d3b1a724f6b1f961efd71c6954c0c9f2ce9e04
Daimaru Tape <Daimaru Tape@DESKTOP-VD3I5ME>
2023-03-30 11:58:00 +0700

    Rewrite

--- app/Http/Livewire/LampiranHistory.php
+++ app/Http/Livewire/LampiranHistory.php
@@ -13,31 +13,34 @@ class LampiranHistory extends Component
 
     public function render()
     {
+
         $users = Lampiran::where('lampirans.created_by', Auth::user()->username)->select('username')
             ->distinct()
             ->get();
 
         $lampirans = Lampiran::join('users', 'users.username', '=', 'lampirans.username')
             ->join('doctors', 'doctors.doctor_nu', '=', 'lampirans.doctor_nu')
-            ->where('lampirans.status', 4)
             // ->whereIn('lampirans.status', [3, 4, 5])
+            ->where('lampirans.status', 4)
             ->where('lampirans.is_expired', '0')
             ->where('lampirans.created_by', Auth::user()->username)
-            ->where(function ($query) {
-                $query
-                    ->where('users.name', '=', $this->username);
+            ->when($this->username, function ($query) {
+                return $query->where('users.name', '=', $this->username);
             })
             ->where(function ($query) {
                 $query
                     ->where('doctors.doctor_nu', 'like', '%' . $this->doctor . '%')
                     ->orWhere('doctors.name', 'like', '%' . $this->doctor . '%');
             })
-            ->select('lampiran_nu', 'users.username', 'users.name', 'doctors.doctor_nu', 'lampirans.updated_at', 'created_by', 'status')
+            ->select('lampiran_nu', 'users.username', 'users.name', 'doctors.doctor_nu', 'created_by', 'lampirans.updated_at','status')
             ->orderBy('lampirans.updated_at', 'DESC')
             ->orderBy('users.name', 'ASC')
             ->orderBy('doctors.name', 'ASC')
             ->distinct()
             ->get();
+
+        // dd($lampirans);
+
 
         return view('livewire.lampiran-history', ['lampirans' => $lampirans, 'users' => $users]);
     }
